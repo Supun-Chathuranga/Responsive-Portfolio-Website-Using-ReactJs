baed01f52e3020dcab4b7cf93f75e0ed9a4a99f9
Supun-Chathuranga <www.supunchathurangacs@gmail.com>
2023-03-29 17:13:14 +0530

    initial

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className="n-wrapper">
+        <div className="n-left">
+            <div className="n-name">Supun</div>
+            <span>toggle</span>
+
+        </div>
+        <div className="n-right">
+            <div className="n-list">
+                <ul>
+                    <li>Home</li>
+                    <li>Services</li>
+                    <li>Experience</li>
+                    <li>Portfolio</li>
+                    <li>Testimonials</li>
+                </ul>
+            </div>
+            <button className="button"></button>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
