a78b63f68855daccad88df89618d152c319ffb3c
Supun-Chathuranga <www.supunchathurangacs@gmail.com>
2023-03-30 16:52:25 +0530

    update

--- src/Components/Work/Work.jsx
+++ src/Components/Work/Work.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
 import './Work.css'
+import Amazon from '../../img/amazon.png'
+import Upwork from '../../img/Upwork.png'
+import Facebook from '../../img/Facebook.png'
+import Fiverr from '../../img/fiverr.png'
+import Shopify from '../../img/Shopify.png'
 
 const Work = () => {
   return (
@@ -15,6 +20,25 @@ const Work = () => {
         </span>
         <button className="button s-button">Download CV</button>
         <div className="blur s-blur" style={{background:"#ABF1FF94"}}></div>
+        </div>
+        <div className="w-right">
+            <div className="w-mainCircle">
+                <div className="w-subCircle">
+                    <img src={Upwork} alt="" />
+                </div>
+                <div className="w-subCircle">
+                    <img src={Amazon} alt="" />
+                </div>
+                <div className="w-subCircle">
+                    <img src={Fiverr} alt="" />
+                </div>
+                <div className="w-subCircle">
+                    <img src={Shopify} alt="" />
+                </div>
+                <div className="w-subCircle">
+                    <img src={Facebook} alt="" />
+                </div>
+            </div>
         </div>
     </div>
   )
